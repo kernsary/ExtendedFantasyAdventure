979b0a4d18c1faa9c982c301ad6523cf84eb7388
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-11-30 23:21:36 +0000

    started Runner, player choosing tested

---
+++ src/main/java/Runner.java
@@ -0,0 +1,65 @@
+import creatures.Creature;
+import players.Healer;
+import players.Magician;
+import players.Player;
+import players.Warrior;
+import rooms.Room;
+import tools.HealingTool;
+import tools.Spell;
+import tools.Weapon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static javax.xml.bind.DatatypeConverter.parseString;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Weapon weapon = new Weapon("Dyrnwyn");
+        Spell spell = new Spell("Fire");
+        Creature creature = new Creature("Twrch Trwyth");
+        HealingTool healingTool = new HealingTool("Bell");
+        ArrayList<Room> rooms = new ArrayList<Room>();
+        Warrior warrior =  new Warrior("Rhydderch", weapon);
+        Healer healer = new Healer("Cynderyn", healingTool);
+        Magician magician = new Magician("Myrddin", spell, creature);
+        Game game = new Game(warrior, rooms);
+
+        System.out.println("Kingdom of Strathclyde, AD 590.");
+        System.out.println("You will travel to enemy kingdoms to find treasure and glory.");
+        System.out.println("But, if the enemy confronts you, you must fight.");
+        System.out.println("Will you survive?");
+        System.out.println();
+        System.out.println("Will you play as:");
+        System.out.println(" - Rhydderch, King of Dumbarton Rock, an mighty warrior.");
+        System.out.println("His strength is in his sword Dyrnwyn, which blazes with fire.");
+        System.out.println("- Cynderyn, the holy man, founder of Glas Cau.");
+        System.out.println("He can heal himself and others with his sacred bell.");
+        System.out.println("- Myrddin, the magician of the wild woods.");
+        System.out.println("He can cast a spell of fire and is protected by wild boar spirit, Twrch Trwyth.");
+        System.out.println();
+        System.out.println("Enter the name of the player you wish to be.");
+
+        String input = scanner.next();
+
+
+        if(input.equals("Rhydderch")){
+//            Warrior warrior = new Warrior("Rhydderch", weapon);
+            game.setPlayer(warrior);
+        }
+        if(input.equals("Cynderyn")){
+//            Healer healer = new Healer("Cynderyn", healingTool);
+            game.setPlayer(healer);
+        }
+        if(input.equals("Myrddin")){
+//            Magician magician = new Magician("Myrddin", spell, creature);
+            game.setPlayer(magician);
+        }
+
+        System.out.println("You are playing as " + game.getPlayer().getName());
+
+    }
+}
